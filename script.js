46f05e93cf7c5090aaae5226fdffec472faaaa66
cherry <90573622+cherrycryy@users.noreply.github.com>
2024-12-16 11:35:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('create-account').addEventListener('click', function() {
+    window.location.href = 'create-account.html'; // Redirect to create account page
+});
+
+document.getElementById('log-in').addEventListener('click', function() {
+    window.location.href = 'login.html'; // Redirect to login page
+});
